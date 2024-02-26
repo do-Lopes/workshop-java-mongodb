f1d392f544dd4e148c4dd3de629c8443a65160fa
Leonardo Lopes <leo2006lopes@gmail.com>
2024-02-26 10:45:12 -0300

    First commit

---
+++ src/test/java/com/wkmongo/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wkmongo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
