e70a163cb751c41ddd09ed352df9636d11e5556b
Leonardo Lopes <leo2006lopes@gmail.com>
2024-02-27 15:30:08 -0300

    User insert, delete and update

--- src/main/java/com/wkmongo/workshopmongo/services/UserService.java
+++ src/main/java/com/wkmongo/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.wkmongo.workshopmongo.services;
 
 import com.wkmongo.workshopmongo.domain.User;
+import com.wkmongo.workshopmongo.dto.UserDTO;
 import com.wkmongo.workshopmongo.repository.UserRepository;
 import com.wkmongo.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,29 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public void delete(String id) {
+        findById(id);
+        repo.deleteById(id);
+    }
+
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
